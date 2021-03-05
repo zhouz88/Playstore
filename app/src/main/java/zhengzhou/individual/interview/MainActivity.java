b7af0c32ca1587721c96e22892cf4ae51e33a58a
kerwinzhou <zhouz88@uw.edu>
2021-03-04 17:56:56 -0800

    has

--- app/src/main/java/zhengzhou/individual/interview/MainActivity.java
+++ app/src/main/java/zhengzhou/individual/interview/MainActivity.java
@@ -3,6 +3,7 @@ package zhengzhou.individual.interview;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
