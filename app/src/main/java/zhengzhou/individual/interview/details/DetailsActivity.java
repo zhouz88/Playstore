d17deb109e0076822df7715f5e471b98b08ec030
zhengzhou <zhezho@amazon.com>
2021-03-17 02:54:01 -0700

    title

--- app/src/main/java/zhengzhou/individual/interview/details/DetailsActivity.java
+++ app/src/main/java/zhengzhou/individual/interview/details/DetailsActivity.java
@@ -3,6 +3,7 @@ package zhengzhou.individual.interview.details;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -23,6 +24,7 @@ public class DetailsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
         toolbar.setTitle("");
+        ((TextView)toolbar.findViewById(R.id.toolbar_news)).setText("Music");
         setSupportActionBar(toolbar);
 
         recyclerView = findViewById(R.id.rec_view);
