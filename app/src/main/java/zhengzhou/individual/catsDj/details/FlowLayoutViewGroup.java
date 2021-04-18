4ae681f3f1f5000e59221fa19c1fe6a5e46392e5
zhouzheng <zhouzheng@zhouzhengs-MacBook-Pro.local>
2021-04-18 12:39:16 -0700

    fix

--- app/src/main/java/zhengzhou/individual/catsDj/details/FlowLayoutViewGroup.java
+++ app/src/main/java/zhengzhou/individual/catsDj/details/FlowLayoutViewGroup.java
@@ -6,7 +6,6 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
